b865b1021333f86bafde22133f2b1d262c2ad87b
dzbeda <44648295+dzbeda@users.noreply.github.com>
2020-02-25 08:18:38 +0200

    Update exercise6-fix.sh

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-#add fix to exercise6-fix here
+total_file_size=0
+dest_folder="${@: -1}"
+
+if [ $HOSTNAME == "server1" ]; then
+ for i in $@; do
+   if [ $i != $dest_folder ]; then
+    file_size=$(wc -c $i | awk '{print $1}')
+    $(scp $i vagrant@server2:/$dest_folder)
+    total_file_size=$((total_file_size + file_size))
+   fi
+done
+else
+ for i in $@; do
+   if [ $i != $dest_folder ]; then
+    file_size=$(wc -c $i | awk '{print $1}')
+    $(scp $i vagrant@server1:/$dest_folder)
+    total_file_size=$((total_file_size + file_size))
+   fi
+done
+fi
+echo "$total_file_size"
